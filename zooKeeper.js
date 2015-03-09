ec25189a80a9384b31c79b57185d8a88fdc17f83
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-03-09 11:51:42 -0400

    adding swich statment, dosn't work

--- zooKeeper.js
+++ zooKeeper.js
@@ -1,3 +1,18 @@
 var sget = require("sget");
 
 var name = sget("You have been given a job as a zoo keeper. You are responsible for a lion. What is his name? ")
+
+careForLion();
+
+function careForLion() {
+	var response = sget("How will you care for %s ?\n 1- feed him\n 2- pet him\n 3- give him a bath\n 4- sing him a song\n 5- go home ",name);
+	switch(response) {
+		case "1":
+		console.log("%s eats his food fast, poor zebra!",name);
+		careForLion();
+		break;
+		default:
+		console.log("Goodbye")
+		break;
+	}
+}
