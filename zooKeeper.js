dd359f432183a53673b72b370a32ca0c5f0e2763
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-03-09 12:01:47 -0400

    added coments to finish later

--- zooKeeper.js
+++ zooKeeper.js
@@ -1,11 +1,11 @@
 var sget = require("sget");
 
-var name = sget("You have been given a job as a zoo keeper. You are responsible for a lion. What is his name? ")
+var name = sget("You have been given a job as a zoo keeper. You are responsible for a lion. What is his name? ");
 
 careForLion();
 
 function careForLion() {
-	var response = sget("How will you care for %s ?\n 1- feed him\n 2- pet him\n 3- give him a bath\n 4- sing him a song\n 5- go home ",name);
+	var response = sget("How will you care for " + name + " ?\n 1- feed him\n 2- pet him\n 3- give him a bath\n 4- sing him a song\n 5- go home ",name);
 	switch(response) {
 		case "1":
 		console.log("%s eats his food fast, poor zebra!",name);
@@ -16,3 +16,7 @@ function careForLion() {
 		break;
 	}
 }
+
+// console.log("%s falls asleep with a smile.",name);
+// console.log("%s hates baths, you get malled!",name);
+// console.log("%s looks like he's dancing",name);
