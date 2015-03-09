9011afdb2f3e536873258ebdcdd93cd2747d8e1c
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-03-09 10:45:18 -0400

    adds second requirement to if else

--- pasword.js
+++ pasword.js
@@ -1,8 +1,13 @@
 var sget = require("sget");
 
-var pasword = sget("Please enter a pasword at least 10 characters long containing at least one capital letter or '!'.")
-if (pasword === pasword.toLowerCase()){
-	console.log ("The password you entered doesn't meet thte requirements.")
-} else{
-	console.log ("Your password has been accepted.")
-}
+enterPassword();
+
+function enterPassword(){
+	var pasword = sget("Please enter a pasword at least 10 characters long containing at least one capital letter or '!'.");
+	if (pasword === pasword.toLowerCase() && pasword === pasword.replace(/!/g, '')){
+		console.log ("The password you entered doesn't meet thte requirements. Please try again.");
+		enterPassword();
+	} else{
+		console.log ("Your password has been accepted.");
+	}
+}	
