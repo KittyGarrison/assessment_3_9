d0fbfc713de3a5539f39f2ac216ba90aa72ed13e
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-03-10 19:03:32 -0400

    adding check for 10 characters

--- pasword.js
+++ pasword.js
@@ -3,10 +3,14 @@ var sget = require("sget");
 enterPassword();
 
 function enterPassword(){
-	var pasword = sget("Please enter a pasword at least 10 characters long containing at least one capital letter or '!'.");
-	if (pasword === pasword.toLowerCase() && pasword === pasword.replace(/!/g, '')){
-		console.log ("The password you entered doesn't meet thte requirements. Please try again.");
-		enterPassword();
+	var password = sget("Please enter a password at least 10 characters long containing at least one capital letter or '!'.").trim();
+	if (password.length < 10){
+		console.log("Your password is not long enough! Try again");
+		return enterPassword();
+	}
+	if (password === password.toLowerCase() && password === password.replace(/!/g, '')){
+		console.log ("The password you entered doesn't meet the requirements. Please try again.");
+		return enterPassword();
 	} else{
 		console.log ("Your password has been accepted.");
 	}
